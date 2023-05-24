5b11a62f7feeeeac784a8b5ed5aa456897d2847a
mahya <beheshtimahya11@gmail.com>
2023-05-24 18:37:04 +0330

    remove tweet

--- src/server/database/TweetDB.java
+++ src/server/database/TweetDB.java
@@ -12,8 +12,8 @@ public class TweetDB extends SQLDB {
     public static void createTweet(String tweetId, String authorId, String context, Object[] attachmentId, Object[] hashtag) {
         LocalDateTime postingTime = LocalDateTime.now();
         try {
-            Array hashtags = connection.createArrayOf("text" , hashtag);
-            Array attachments = connection.createArrayOf("text" , attachmentId);
+            Array hashtags = connection.createArrayOf("text", hashtag);
+            Array attachments = connection.createArrayOf("text", attachmentId);
             //TODO attachmenst and id
             preparedStatement = connection.prepareStatement("INSERT INTO profile (id, athour ,favestar, hashtag, attachmenst , poatingtime) VALUES (?,?,?,?,?,?)");
             preparedStatement.setString(1, tweetId);
@@ -21,7 +21,7 @@ public class TweetDB extends SQLDB {
             preparedStatement.setBoolean(3, false);
             preparedStatement.setArray(4, hashtags);
             preparedStatement.setArray(5, attachments);
-            preparedStatement.setTimestamp(6,Timestamp.valueOf(LocalDateTime.now()));
+            preparedStatement.setTimestamp(6, Timestamp.valueOf(LocalDateTime.now()));
 
             ResultSet resultSet = preparedStatement.executeQuery();
             resultSet.next();
@@ -29,7 +29,19 @@ public class TweetDB extends SQLDB {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-
-
     }
+
+
+    public static void removeTweet(int messageId) {
+        try {
+            preparedStatement = connection.prepareStatement("DELETE FROM tweet WHERE id = ?");
+            preparedStatement.setInt(1 , messageId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 }
