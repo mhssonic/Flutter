ac4d7476972da9d3d9288bde84d57db4ad8ed08a
mahya <beheshtimahya11@gmail.com>
2023-05-24 15:17:17 +0330

    AppendMessageToChatBox

--- src/server/database/ChatBoxDB.java
+++ src/server/database/ChatBoxDB.java
@@ -6,4 +6,9 @@ import java.sql.SQLException;
 
 public class ChatBoxDB extends SQLDB{
 
+    public static void AppendMessageToChatBox(String userId , String messageId){
+        String chatBox = userId;//TODO handel
+        SQLDB.appendToArrayField("chat_box" , chatBox , "message_id", messageId);
+    }
+
 }
