efc9cd8fe73d30447520c31689a4a104b62c9094
mahya <beheshtimahya11@gmail.com>
2023-05-25 18:18:27 +0330

    retweet but not complete

--- src/server/message/Tweet/Retweet.java
+++ src/server/message/Tweet/Retweet.java
@@ -1,9 +1,17 @@
 package server.message.Tweet;
 
+import server.database.RetweetDB;
+import server.enums.error.ErrorType;
+
 public class Retweet {
-    private int retweetId;
+    private int retweetId;//TODO ?
     private int retweetedMessageId;
     private int retweeterId;
+
+    public static ErrorType retweet( int retweetedMessageId, int retweeterId) {
+        int retweetId = RetweetDB.createRetweet(retweetedMessageId, retweeterId);
+        return ErrorType.SUCCESS;
+    }
 
 }
 
