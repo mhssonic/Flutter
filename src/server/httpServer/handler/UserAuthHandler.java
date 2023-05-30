d232c9f7fecb1a57686067d1921ff05af5ef9e8f
mhsso <mhs.sonic13@gmail.com>
2023-05-30 19:47:37 +0330

    complete signInHandler

--- src/server/httpServer/handler/UserAuthHandler.java
+++ src/server/httpServer/handler/UserAuthHandler.java
@@ -5,14 +5,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import server.Tools;
 import server.database.UserDB;
+import server.httpServer.FlutterHttpServer;
+import server.user.userController;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.time.LocalDate;
 
 public class UserAuthHandler {
-    final static int VALID_TOKEN = 30;
+    public final static int VALID_TOKEN = 30;
     public static void signInHandler(HttpExchange exchange){
         ObjectMapper objectMapper = new ObjectMapper();
         try {
@@ -20,9 +21,13 @@ public class UserAuthHandler {
             String username = jsonNode.get("username").asText();
             String password = jsonNode.get("password").toString();
 
-            String id = UserDB.matchUserPass(username, password);
+            String jwt = userController.signIn(username, password);
 
-            String jwt = Tools.creatJWT(id, LocalDate.now(), LocalDate.now().plusDays(VALID_TOKEN), "hiiiiiiiiish be kasi nago ino");//TODO move key to database
+            if(jwt == null){
+                FlutterHttpServer.sendNotOkResponse(exchange, HttpURLConnection.HTTP_OK);
+                //TODO return something
+                return;
+            }
             exchange.getResponseHeaders().add("Set-Cookie", "token=" + jwt);//TODO ummm im not sure that java can handle cookies like that (test it its not that hard and you know it)
             exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, -1);
         } catch (IOException e) {
