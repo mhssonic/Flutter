3a3db8ac021d691aecc1d1819b78738363181858
mahya <beheshtimahya11@gmail.com>
2023-05-31 17:58:01 +0330

    sign up handler

---
+++ src/server/user/SignUpForm.java
@@ -0,0 +1,112 @@
+package server.user;
+
+public class SignUpForm {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String country;
+    private String birthdate;
+    private String biography;
+    private String avatarPath;
+    private String headerPath;
+    private String userName;
+    private String password;
+    private String confirmPassword;
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public String getAvatarPath() {
+        return avatarPath;
+    }
+
+    public void setAvatarPath(String avatarPath) {
+        this.avatarPath = avatarPath;
+    }
+
+    public String getHeaderPath() {
+        return headerPath;
+    }
+
+    public void setHeaderPath(String headerPath) {
+        this.headerPath = headerPath;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- src/server/user/User.java
+++ src/server/user/User.java
@@ -11,4 +11,60 @@ public class User {
     HashSet<User> following = new HashSet<>();
     HashSet<User> follower = new HashSet<>();
     HashSet<User> blocked = new HashSet<>();
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getProfileId() {
+        return profileId;
+    }
+
+    public void setProfileId(String profileId) {
+        this.profileId = profileId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public HashSet<User> getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(HashSet<User> following) {
+        this.following = following;
+    }
+
+    public HashSet<User> getFollower() {
+        return follower;
+    }
+
+    public void setFollower(HashSet<User> follower) {
+        this.follower = follower;
+    }
+
+    public HashSet<User> getBlocked() {
+        return blocked;
+    }
+
+    public void setBlocked(HashSet<User> blocked) {
+        this.blocked = blocked;
+    }
 }
