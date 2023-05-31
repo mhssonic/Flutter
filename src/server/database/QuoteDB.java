0079e955a318809c61171788c457a690cc57d413
mhsso <mhs.sonic13@gmail.com>
2023-05-31 20:17:39 +0330

    resolve conflict

--- src/server/database/QuoteDB.java
+++ src/server/database/QuoteDB.java
@@ -1,13 +1,16 @@
 package server.database;
 
+import server.message.tweet.Quote;
+import server.message.tweet.Tweet;
+
 import java.sql.Array;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
-public class QuoteDB extends SQLDB{
-    public static int createQuote(int authorId, String context, Integer[] attachmentId, Integer[] hashtag, LocalDateTime postingTime , int quotedMessageID) {
+public class QuoteDB extends TweetDB {
+    public static int createQuote(int authorId, String context, Integer[] attachmentId, Integer[] hashtag, LocalDateTime postingTime, int quotedMessageID) {
         try {
             Array hashtags = connection.createArrayOf("INT", hashtag);
             Array attachments = connection.createArrayOf("INT", attachmentId);
@@ -19,11 +22,36 @@ public class QuoteDB extends SQLDB{
             preparedStatement.setArray(4, attachments);
             preparedStatement.setTimestamp(5, Timestamp.valueOf(postingTime));
             preparedStatement.setString(6, context);
-            preparedStatement.setInt(7,quotedMessageID);
+            preparedStatement.setInt(7, quotedMessageID);
 
             ResultSet resultSet = preparedStatement.executeQuery();
             resultSet.next();
             return resultSet.getInt("id");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Tweet getTweet(int messageId) {
+
+        try {
+            ResultSet resultSet = getResultSet("quote", messageId);
+            if (!resultSet.next()) return null;
+
+            int author = resultSet.getInt("author");
+            String context = resultSet.getString("context");
+            Object[] attachmentId = (Object[]) (resultSet.getArray("attachment").getArray());
+//            Attachment[] attachments = AttachmentDB.getAttachment(Object[]attachmentId);
+            int retweet = resultSet.getInt("retweet");
+            int likes = sizeOfArrayField("tweet", messageId, "likes");
+            Object[] commentId = (Object[]) (resultSet.getArray("comments").getArray());
+//            Comment[] comments = CommentDB.getComments(Object[]commentId);
+            Object[] hashtag = (Object[]) (resultSet.getArray("comments").getArray());
+            LocalDateTime postingTime = resultSet.getTimestamp("postingTime").toLocalDateTime();
+            int quotedMessageId = resultSet.getInt("quoted_message_id");
+
+            Quote quote = new Quote(messageId, author, context, postingTime, attachmentId, likes, quotedMessageId);
+            return quote;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
